d38b151a2ae567953ab7afb0204aaab8fe2b48f6
Danh Nguyen <danh.nguyen@menlosecurity.com>
2022-07-06 14:03:16 +0700

    first commit

---
+++ src/main/java/com/java/demojenkins/DemoJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.java.demojenkins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class DemoJenkinsApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(DemoJenkinsApplication.class);
+
+	@PostConstruct
+	public void intt(){
+		logger.info("Application start...");
+	}
+
+	public static void main(String[] args) {
+
+		logger.info("Application excuted...");
+		SpringApplication.run(DemoJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/demojenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.java.demojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(DemoJenkinsApplication.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("Test case excuting...");
+		assertEquals(true, true);
+	}
+
+}
